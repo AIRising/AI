ea9c7175d87432ce3fb02e5a1b041c16575c397a
Shemp <ultimategarlough@gmail.com>
2015-11-03 00:45:38 +0000

    Converted all labs, and through labs up to lab 3 - started lab 4

---
+++ lab0/Lab2/lab2/lab2.py
@@ -0,0 +1,152 @@
+﻿# Fall 2012 6.034 Lab 2: Search
+#
+# Your answers for the true and false questions will be in the following form.  
+# Your answers will look like one of the two below:
+#ANSWER1 = True
+#ANSWER1 = False
+
+# 1: True or false - Hill Climbing search is guaranteed to find a solution
+#    if there is a solution
+ANSWER1 = False
+
+# 2: True or false - Best-first search will give an optimal search result
+#    (shortest path length).
+#    (If you don't know what we mean by best-first search, refer to
+#     http://courses.csail.mit.edu/6.034f/ai3/ch4.pdf (page 13 of the pdf).)
+ANSWER2 = False
+
+# 3: True or false - Best-first search and hill climbing make use of
+#    heuristic values of nodes.
+ANSWER3 = True
+
+# 4: True or false - A* uses an extended-nodes set.
+ANSWER4 = True
+
+# 5: True or false - Breadth first search is guaranteed to return a path
+#    with the shortest number of nodes.
+ANSWER5 = True
+
+# 6: True or false - The regular branch and bound uses heuristic values
+#    to speed up the search for an optimal path.
+ANSWER6 = False
+
+# Import the Graph data structure from 'search.py'
+# Refer to search.py for documentation
+from search import Graph
+
+## Optional Warm-up: BFS and DFS
+# If you implement these, the offline tester will test them.
+# If you don't, it won't.
+# The online tester will not test them.
+
+def bfs(graph, start, goal):
+    raise NotImplementedError
+
+## Once you have completed the breadth-first search,
+## this part should be very simple to complete.
+def dfs(graph, start, goal):
+    raise NotImplementedError
+
+
+## Now we're going to add some heuristics into the search.  
+## Remember that hill-climbing is a modified version of depth-first search.
+## Search direction should be towards lower heuristic values to the goal.
+def hill_climbing(graph, start, goal):
+    #graph.get_heuristic(start, goal) .get_connected_nodes(node) .get_edge(node1,node2) .are_connected(node1, node2) graph.is_valid_path(path):
+    currentNode = start
+    path = [start]
+    agenda = []
+    while(currentNode is not None):
+        connectedNodes = graph.get_connected_nodes(currentNode)
+        distanceDict = []
+        for pathExtension in sorted((node for node in connectedNodes if (node not in agenda and node not in path)), key=lambda x: (graph.get_heuristic(x, goal)), reverse=True):
+            #Add each of these guys to the agenda in order
+            pathCopy = path[:]
+            if(pathExtension == goal):
+                path.append(pathExtension)
+                return path
+            pathCopy.append(pathExtension)
+            agenda.append([pathExtension, pathCopy])
+        if(len(agenda) > 0):
+            nextOnAgenda = agenda.pop()
+            currentNode = nextOnAgenda[0][:]
+            path = nextOnAgenda[1][:]
+        else:
+            currentNode = None
+    return path
+
+#Expected: ['F', 'B', 'D', 'C', 'E', 'G']
+
+## Now we're going to implement beam search, a variation on BFS
+## that caps the amount of memory used to store paths.  Remember,
+## we maintain only k candidate paths of length n in our agenda at any time.
+## The k top candidates are to be determined using the 
+## graph get_heuristic function, with lower values being better values.
+def beam_search(graph, start, goal, beam_width):
+    raise NotImplementedError
+
+## Now we're going to try optimal search.  The previous searches haven't
+## used edge distances in the calculation.
+
+## This function takes in a graph and a list of node names, and returns
+## the sum of edge lengths along the path -- the total distance in the path.
+
+def path_length(graph, node_names):
+    pathLen = 0
+    numNodes = len(node_names)
+    if(numNodes > 1):
+        for index in range(numNodes-1):
+            #Get this node in the list and then figure out how to get to the next node in the list
+            node1 = node_names[index]
+            node2 = node_names[index+1]
+            edgeFound = graph.get_edge(node1,node2)
+            if(not edgeFound is None):
+                pathLen += edgeFound.length
+    return pathLen
+
+
+def branch_and_bound(graph, start, goal):
+    raise NotImplementedError
+
+def a_star(graph, start, goal):
+    #graph.get_heuristic(start, goal) .get_connected_nodes(node) .get_edge(node1,node2) .are_connected(node1, node2) graph.is_valid_path(path):
+    #each time we run through a star, we're going to calculate the total distance
+    currentNode = start
+    path = [start]
+    extendedSet = []
+    agenda = []
+    while(currentNode is not None):
+        connectedNodes = graph.get_connected_nodes(currentNode)
+        distanceDict = []
+        for pathExtension in (node for node in connectedNodes if (node not in extendedSet)):
+            #Add each of these guys to the agenda in order
+            totalDistanceEstimate = graph.get_heuristic(pathExtension, goal) + path_length(graph,path+[pathExtension])
+            if(pathExtension == goal):
+                path.append(pathExtension)
+                return path
+            agenda.append([totalDistanceEstimate, path+[pathExtension]])
+        if(len(agenda) > 0):
+            agenda.sort(key=lambda x: (x[0], x[1]), reverse=True)
+            nextOnAgenda = agenda.pop()
+            currentNode = nextOnAgenda[1][-1]
+            extendedSet.append(currentNode)
+            path = nextOnAgenda[1][:]
+        else:
+            currentNode = None
+    return path
+
+
+## It's useful to determine if a graph has a consistent and admissible
+## heuristic.  You've seen graphs with heuristics that are
+## admissible, but not consistent.  Have you seen any graphs that are
+## consistent, but not admissible?
+
+def is_admissible(graph, goal):
+    raise NotImplementedError
+
+def is_consistent(graph, goal):
+    raise NotImplementedError
+
+HOW_MANY_HOURS_THIS_PSET_TOOK = ''
+WHAT_I_FOUND_INTERESTING = ''
+WHAT_I_FOUND_BORING = ''
---
+++ lab0/Lab3/lab3/lab3/lab3.py
@@ -0,0 +1,272 @@
+﻿# 6.034 Fall 2010 Lab 3: Games
+# Name: <Your Name>
+# Email: <Your Email>
+
+from util import INFINITY
+
+### 1. Multiple choice
+
+# 1.1. Two computerized players are playing a game. Player MM does minimax
+#      search to depth 6 to decide on a move. Player AB does alpha-beta
+#      search to depth 6.
+#      The game is played without a time limit. Which player will play better?
+#
+#      1. MM will play better than AB.
+#      2. AB will play better than MM.
+#      3. They will play with the same level of skill.
+ANSWER1 = 3
+
+# 1.2. Two computerized players are playing a game with a time limit. Player MM
+# does minimax search with iterative deepening, and player AB does alpha-beta
+# search with iterative deepening. Each one returns a result after it has used
+# 1/3 of its remaining time. Which player will play better?
+#
+#   1. MM will play better than AB.
+#   2. AB will play better than MM.
+#   3. They will play with the same level of skill.
+ANSWER2 = 2
+
+### 2. Connect Four
+from connectfour import *
+from basicplayer import *
+from util import *
+import tree_searcher
+
+## This section will contain occasional lines that you can uncomment to play
+## the game interactively. Be sure to re-comment them when you're done with
+## them.  Please don't turn in a problem set that sits there asking the
+## grader-bot to play a game!
+## 
+## Uncomment this line to play a game as white:
+#run_game(human_player, basic_player)
+
+## Uncomment this line to play a game as black:
+#run_game(basic_player, human_player)
+
+## Or watch the computer play against itself:
+#run_game(basic_player, basic_player)
+
+## Change this evaluation function so that it tries to win as soon as possible,
+## or lose as late as possible, when it decides that one side is certain to win.
+## You don't have to change how it evaluates non-winning positions.
+
+#Leave the "normal" values (the ones that are like 1 or -2, not 1000 or -1000) alone. You don't
+#need to change how the procedure evaluates positions that aren't guaranteed wins or losses.
+# Indicate a certain win with a value that is greater than or equal to 1000, and a certain loss with a
+#value that is less than or equal to -1000.
+# Remember, focus_evaluate should be very simple. So don't introduce any fancy heuristics
+#in here, save your ideas for when you implement better_evaluate later on. 
+
+def focused_evaluate(board):
+    """
+    Given a board, return a numeric rating of how good
+    that board is for the current player.
+    A return value >= 1000 means that the current player has won;
+    a return value <= -1000 means that the current player has lost
+    """    
+    if board.is_game_over():
+        # If the game has been won, we know that it must have been
+        # won or ended by the previous move.
+        # The previous move was made by our opponent.
+        # Therefore, we can't have won, so return -1000.
+        # (note that this causes a tie to be treated like a loss)
+        score = -1000
+    else:
+        score = board.longest_chain(board.get_current_player_id()) * 10
+        # Prefer having your pieces in the center of the board.
+        for row in range(6):
+            for col in range(7):
+                if board.get_cell(row, col) == board.get_current_player_id():
+                    score -= abs(3-col)
+                elif board.get_cell(row, col) == board.get_other_player_id():
+                    score += abs(3-col)
+
+    return score
+
+
+## Create a "player" function that uses the focused_evaluate function
+quick_to_win_player = lambda board: minimax(board, depth=4,
+                                            eval_fn=focused_evaluate)
+
+class PlayTree:
+    def __init__(self, board, column, depthToExtendTo, is_maximizers_turn, currentDepth, eval_fn, get_next_moves_fn, is_terminal_fn, alpha, beta):
+        self.children = []
+        self.board = board
+        self.depthToExtendTo = depthToExtendTo
+        self.is_maximizers_turn = is_maximizers_turn
+        self.currentDepth = currentDepth
+        self.eval_fn = eval_fn
+        self.get_next_moves_fn = get_next_moves_fn
+        self.is_terminal_fn = is_terminal_fn
+        self.alpha = alpha
+        self.beta = beta
+        self.columnMove = column
+
+    def Beta(self):
+        return self.beta
+    
+    def Column(self):
+        return self.columnMove
+
+    def BestMove(self):
+        bestBeta = -10000
+        bestMove = -1
+        if(len(self.children) == 0):
+            self.AlphaBetaSearch()
+        if(not self.children is None):
+            for child in self.children:
+                if(child.Beta() > bestBeta):
+                    bestBeta = child.Beta()
+                    bestMove = child.Column()
+        return bestMove
+
+    def AlphaBetaSearch(self):
+        if(not self.is_terminal_fn(1, self.board)):
+            self.next_moves = self.get_next_moves_fn(self.board)
+            if(self.currentDepth < self.depthToExtendTo):
+                if(self.is_maximizers_turn):
+                    v = -10000
+                    #We still need to extend the tree deeper, so we're just going to look for moves, and pass the extension to the next level down
+                    for move in self.next_moves:
+                        moveTree = PlayTree(self.board.do_move(move[0]), move[0], self.depthToExtendTo, not self.is_maximizers_turn, self.currentDepth+1, self.eval_fn, self.get_next_moves_fn, self.is_terminal_fn, self.alpha, self.beta)
+                        self.children.append(moveTree)
+                        alphaBetaVal = moveTree.AlphaBetaSearch()
+                        v = max(alphaBetaVal, v)
+                        self.alpha = max(self.alpha, v)
+                        if(self.beta <= self.alpha):
+                            break
+                    return v
+                else:
+                    v = 10000
+                    #We still need to extend the tree deeper, so we're just going to look for moves, and pass the extension to the next level down
+                    for move in self.next_moves:
+                        moveTree = PlayTree(self.board.do_move(move[0]), move[0], self.depthToExtendTo, not self.is_maximizers_turn, self.currentDepth+1, self.eval_fn, self.get_next_moves_fn, self.is_terminal_fn, self.alpha, self.beta)
+                        self.children.append(moveTree)
+                        alphaBetaVal = moveTree.AlphaBetaSearch()
+                        v = min(alphaBetaVal, v)
+                        self.beta = min(self.beta, v)
+                        if(self.beta <= self.alpha):
+                            break
+                    return v
+            if(self.currentDepth == self.depthToExtendTo):
+                return self.eval_fn(self.board)
+        else:
+            return self.eval_fn(self.board)
+
+## You can try out your new evaluation function by uncommenting this line:
+#run_game(basic_player, quick_to_win_player)
+
+## Write an alpha-beta-search procedure that acts like the minimax-search
+## procedure, but uses alpha-beta pruning to avoid searching bad ideas
+## that can't improve the result. The tester will check your pruning by
+## counting the number of static evaluations you make.
+##
+## You can use minimax() in basicplayer.py as an example.
+def alpha_beta_search(board, depth,
+                      eval_fn,
+                      # NOTE: You should use get_next_moves_fn when generating
+                      # next board configurations, and is_terminal_fn when
+                      # checking game termination.
+                      # The default functions set here will work
+                      # for connect_four.
+                      get_next_moves_fn=get_all_next_moves,
+		      is_terminal_fn=is_terminal):
+   
+    playTree = PlayTree(board, 0, depth, True, -1, eval_fn, get_next_moves_fn, is_terminal_fn, -10000, 10000)
+
+    return playTree.BestMove()
+
+## Now you should be able to search twice as deep in the same amount of time.
+## (Of course, this alpha-beta-player won't work until you've defined
+## alpha-beta-search.)
+alphabeta_player = lambda board: alpha_beta_search(board,
+                                                   depth=8,
+                                                   eval_fn=focused_evaluate)
+
+## This player uses progressive deepening, so it can kick your ass while
+## making efficient use of time:
+ab_iterative_player = lambda board: \
+    run_search_function(board,
+                        search_fn=alpha_beta_search,
+                        eval_fn=focused_evaluate, timeout=5)
+#run_game(human_player, alphabeta_player)
+
+## Finally, come up with a better evaluation function than focused-evaluate.
+## By providing a different function, you should be able to beat
+## simple-evaluate (or focused-evaluate) while searching to the
+## same depth.
+
+def better_evaluate(board):
+    raise NotImplementedError
+
+# Comment this line after you've fully implemented better_evaluate
+better_evaluate = memoize(basic_evaluate)
+
+# Uncomment this line to make your better_evaluate run faster.
+# better_evaluate = memoize(better_evaluate)
+
+# For debugging: Change this if-guard to True, to unit-test
+# your better_evaluate function.
+if False:
+    board_tuples = (( 0,0,0,0,0,0,0 ),
+                    ( 0,0,0,0,0,0,0 ),
+                    ( 0,0,0,0,0,0,0 ),
+                    ( 0,2,2,1,1,2,0 ),
+                    ( 0,2,1,2,1,2,0 ),
+                    ( 2,1,2,1,1,1,0 ),
+                    )
+    test_board_1 = ConnectFourBoard(board_array = board_tuples,
+                                    current_player = 1)
+    test_board_2 = ConnectFourBoard(board_array = board_tuples,
+                                    current_player = 2)
+    # better evaluate from player 1
+    print("%s => %s" %(test_board_1, better_evaluate(test_board_1)))
+    # better evaluate from player 2
+    print("%s => %s" %(test_board_2, better_evaluate(test_board_2)))
+
+## A player that uses alpha-beta and better_evaluate:
+your_player = lambda board: run_search_function(board,
+                                                search_fn=alpha_beta_search,
+                                                eval_fn=better_evaluate,
+                                                timeout=5)
+
+#your_player = lambda board: alpha_beta_search(board, depth=4,
+#                                              eval_fn=better_evaluate)
+
+## Uncomment to watch your player play a game:
+#run_game(your_player, your_player)
+
+## Uncomment this (or run it in the command window) to see how you do
+## on the tournament that will be graded.
+#run_game(your_player, basic_player)
+
+## These three functions are used by the tester; please don't modify them!
+def run_test_game(player1, player2, board):
+    assert isinstance(globals()[board], ConnectFourBoard), "Error: can't run a game using a non-Board object!"
+    return run_game(globals()[player1], globals()[player2], globals()[board])
+    
+def run_test_search(search, board, depth, eval_fn):
+    assert isinstance(globals()[board], ConnectFourBoard), "Error: can't run a game using a non-Board object!"
+    return globals()[search](globals()[board], depth=depth,
+                             eval_fn=globals()[eval_fn])
+
+## This function runs your alpha-beta implementation using a tree as the search
+## rather than a live connect four game.   This will be easier to debug.
+def run_test_tree_search(search, board, depth):
+    return globals()[search](globals()[board], depth=depth,
+                             eval_fn=tree_searcher.tree_eval,
+                             get_next_moves_fn=tree_searcher.tree_get_next_move,
+                             is_terminal_fn=tree_searcher.is_leaf)
+    
+## Do you want us to use your code in a tournament against other students? See
+## the description in the problem set. The tournament is completely optional
+## and has no effect on your grade.
+COMPETE = (None)
+
+## The standard survey questions.
+HOW_MANY_HOURS_THIS_PSET_TOOK = ""
+WHAT_I_FOUND_INTERESTING = ""
+WHAT_I_FOUND_BORING = ""
+NAME = ""
+EMAIL = ""
+
